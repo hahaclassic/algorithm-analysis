9234d43c98e208007c96a1240a0fb863112af5aa
hahaclassic <gavrilyukvlad221@gmail.com>
2024-10-27 20:43:48 +0300

    added ready/app-cli-debug target

--- levenshtein/iu7-algan-cicd-student.mk
+++ levenshtein/iu7-algan-cicd-student.mk
@@ -6,15 +6,17 @@ $(READY_DIR)/report.pdf: $(READY_DIR)
 $(READY_DIR)/stud-unit-test-report-prev.json: $(READY_DIR)
 	cp test/stud-unit-test-report-prev.json $(READY_DIR)/stud-unit-test-report-prev.json
 
+$(READY_DIR)/stud-unit-test-report.json: $(READY_DIR)
+	python3 ./unit.py --output=$(READY_DIR)/stud-unit-test-report.json
+
 $(READY_DIR)/main-cli-debug.py: $(READY_DIR)
 	cp -rf src/* $(READY_DIR)/
 	mv $(READY_DIR)/main.py $(READY_DIR)/main-cli-debug.py
 
-$(READY_DIR)/stud-unit-test-report.json: $(READY_DIR)
-	python3 ./unit.py --output=$(READY_DIR)/stud-unit-test-report.json
-
 $(READY_DIR):
 	@mkdir -p ./ready
+
+$(READY_DIR)/app-cli-debug:
 
 .PHONY: clean
 clean:
