47f24e5d4a9afa744d681b30da2ee6f71094e7a4
hahaclassic <gavrilyukvlad221@gmail.com>
2024-11-04 22:17:36 +0300

    updated code style and added script for charts

--- levenshtein/src/levenshtein.py
+++ levenshtein/src/levenshtein.py
@@ -62,8 +62,7 @@ def RecursiveCacheLevenshtein(s1: str, s2: str, memo: dict = None) -> int:
 
 def DynamicLevenshtein(s1: str, s2: str) -> int:
     length1, length2 = len(s1), len(s2)
-
-    if len(s1) == 0 or len(s2) == 0:
+    if length1 == 0 or length2 == 0:
         return abs(length1 - length2)
     
     matrix = getInitialMatrix(length1, length2)
@@ -75,16 +74,14 @@ def DynamicLevenshtein(s1: str, s2: str) -> int:
             matrix[i][j] = min(
                 matrix[i - 1][j] + DELETE_COST, 
                 matrix[i][j - 1] + INSERT_COST,
-                matrix[i - 1][j - 1] + cost
-            )
+                matrix[i - 1][j - 1] + cost)
 
     return matrix[length1][length2]
 
 
 def DynamicDamerauLevenshtein(s1: str, s2: str) -> int:
     length1, length2 = len(s1), len(s2)
-
-    if len(s1) == 0 or len(s2) == 0:
+    if length1 == 0 or length2 == 0:
         return abs(length1 - length2)
 
     matrix = getInitialMatrix(length1, length2)
@@ -96,15 +93,13 @@ def DynamicDamerauLevenshtein(s1: str, s2: str) -> int:
             matrix[i][j] = min(
                 matrix[i - 1][j] + DELETE_COST,
                 matrix[i][j - 1] + INSERT_COST,
-                matrix[i - 1][j - 1] + cost
-            )
+                matrix[i - 1][j - 1] + cost)
 
             if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] \
                 and s1[i - 2] == s2[j - 1]:
         
                 matrix[i][j] = min(
                     matrix[i][j], 
-                    matrix[i - 2][j - 2] + TRANSPOSITION_COST
-                )
+                    matrix[i - 2][j - 2] + TRANSPOSITION_COST)
 
     return matrix[length1][length2]
