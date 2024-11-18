7ece79f99ba0986b473a923c860473a8a3dbb1ac
hahaclassic <gavrilyukvlad221@gmail.com>
2024-11-19 01:57:02 +0300

    added report

--- 04_multithreaded_web_scraper/code/hist.py
+++ 04_multithreaded_web_scraper/code/hist.py
@@ -2,18 +2,18 @@ import matplotlib.pyplot as plt
 
 # Data
 threads = [1, 2, 4, 8, 16, 32, 48]
-pages_per_sec = [1.10, 1.50, 3.00, 4.50, 7.65, 12.15, 9.95]
+pages_per_sec = [1.10, 2.22, 3.43, 5.10, 6.31, 7.64, 6.92]
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.bar(threads, pages_per_sec, color='skyblue', edgecolor='black', width=0.7)  # Увеличиваем ширину столбцов
-plt.xlabel('Number of Threads')
-plt.ylabel('Pages per Second')
-plt.title('Performance vs. Number of Threads')
+plt.bar(threads, pages_per_sec, color='skyblue', edgecolor='black', zorder=2) 
+plt.xlabel('Количество потоков, шт')
+plt.ylabel('Скорость загрузки страниц, кол-во страниц/сек')
+plt.title('Зависимость скорости загрузки страниц от количества потоков')
 plt.xticks(threads)
-
+# plt.xscale("log", base=2)
 # Add grid on the background
-plt.grid(axis='y', color='gray', linestyle='--', linewidth=0.5)
+plt.grid(axis='both', color='gray', linestyle='--', linewidth=0.5, zorder=1)
 
 # Display the plot
 plt.show()
