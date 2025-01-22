cc67df3388e0ccd4c19f2f0c7eb86bd416ccf213
hahaclassic <gavrilyukvlad221@gmail.com>
2025-01-22 08:15:01 +0300

    updated function

--- 07_graphs/main.go
+++ 07_graphs/main.go
@@ -1,38 +1,45 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log/slog"
-	"os"
-	"path/filepath"
-	"time"
 )
 
 // code for analysis
+func (p Parser) extractIngredients(doc *goquery.Document) []*models.Ingredient {
+	var ingredients []*models.Ingredient
+	seen := make(map[string]struct{})
 
-func (p *PipelineService) processTasks(ctx context.Context, tasks []*models.Task) { // 1
-	for _, task := range tasks { // 2 - Первый цикл: итерация по списку задач
-		// Вложенный цикл: обработка элементов Meta
-		metaItems, ok := task.Meta.([]string) // 3
-		if !ok {                              // 4
-			slog.Error("processTasks", "err", "type assertion error") // 5
-			continue                                                  // 6
-		} // 7
+	// Находим все контейнеры с ингредиентами
+	containers := doc.Find("div.ingredient-list")
+	for i := 0; i < containers.Length(); i++ {
+		container := containers.Eq(i)
 
-		for _, item := range metaItems { // 8 - Вложенный цикл: итерация по элементам Meta
-			content, err := os.ReadFile(filepath.Join(p.Conf.SourceDir, item)) // 9
-			if err != nil {                                                    // 10
-				slog.Error("processTasks", "err", fmt.Errorf("failed to read file '%s': %w", item, err)) // 11
-			{
+		// Находим все скрытые элементы внутри контейнера
+		hiddenInputs := container.Find("input[type='hidden'][data-declensions]")
+		for j := 0; j < hiddenInputs.Length(); j++ {
+			input := hiddenInputs.Eq(j)
 
-			// Дополнительная обработка содержимого файла
-			task.Stages = append(task.Stages, &models.StageInfo{ // 14
-				Name:   "processItem", // 15
-				Queued: time.Now(),    // 16
-			}) // 17
+			// Извлекаем название ингредиента
+			ingredientName := input.Parent().Find("span.recipe_ingredient_title").Text()
 
-			slog.Info("processTasks", "info", fmt.Sprintf("Processed item: %s", item)) // 18
-		} // 19
-	} // 20
-} // 21
+			if _, ok := seen[ingredientName]; ingredientName != "" && !ok {
+				seen[ingredientName] = struct{}{}
+				unit := p.extractUnit(input)
+				quantity := p.extractQuantity(input)
+
+				// Проверка на валидность данных
+				if unit == "" || quantity <= 0 {
+					fmt.Printf("Warning: invalid ingredient data for %s\n", ingredientName)
+				}
+
+				ingredients = append(ingredients, &models.Ingredient{
+					Name:     ingredientName,
+					Unit:     unit,
+					Quantity: quantity,
+				})
+			}
+		}
+	}
+
+	return ingredients
+}
